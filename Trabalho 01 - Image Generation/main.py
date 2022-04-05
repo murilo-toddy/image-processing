9431cb09defed8bb08d87a197538048972a42936
murilo-toddy <murilo.todao@usp.br>
2022-04-05 16:03:17 -0300

    Implement color quantization

--- Trabalho 01 - Image Generation/main.py
+++ Trabalho 01 - Image Generation/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import random
 
 
+# Return function that should be perfomed depending on x, y coordinate
 def get_transformation_function(operation):
     if operation == 1:
         def get_value(x, y):
@@ -21,28 +22,57 @@ def get_transformation_function(operation):
         def get_value(x, y):
             random.random()
 
-    return get_value if get_value else None
+    return get_value
 
 
 def generate_custom_image(operation):
-    transformed_image = np.zeros((scene_size, scene_size), float)
+    generated_image = np.zeros((scene_size, scene_size), float)
     random.seed(random_seed)
-    print(transformed_image)
 
-    operation_function = get_transformation_function(operation)
+    if operation != 5:
+        operation_function = get_transformation_function(operation)
+        for x in range(0, scene_size):
+            for y in range(0, scene_size):
+                generated_image[x, y] = operation_function(x, y)
 
-    for x in range(0, transformed_image.shape[0]):
-        for y in range(0, transformed_image.shape[1]):
-            transformed_image[x, y] = operation_function(x, y)
+    else:
+        # Randomwalk function
+        # Perform randomwalk operation
+        x, y = 0, 0
+        generated_image[x, y] = 1
+        for iteration in range(0, 1 + scene_size ** 2):
+            # Find walking direcion
+            dx = random.randint(-1, 1)
+            dy = random.randint(-1, 1)
+            x = (x + dx) % scene_size
+            y = (y + dy) % scene_size
+            generated_image[x, y] = 1
 
-    plt.plot(transformed_image)
-    plt.show()
-    return transformed_image
+    return generated_image
+
+
+def normalize_image(image):
+    max_value = np.max(image)
+    min_value = np.min(image)
+    image = (2 ** 16 - 1) * ((image - min_value) / (max_value - min_value))
+    return image
+
+
+def quantize_image(image):
+    quantized_image = np.zeros((image_size, image_size), np.uint8)
+    step_size = int(np.floor(scene_size / image_size))
+    correction_factor = (2 ** 8 - 1) / (2 ** 16 - 1)
+    for x in range(0, image_size):
+        for y in range(0, image_size):
+            quantized_image[x, y] = correction_factor * image[x * step_size, y * step_size]
+            quantized_image[x, y] = quantized_image[x, y] >> (8 - bits_per_pixel)
+
+    return quantized_image
 
 
 def root_squared_error(image1, image2):
     # Calculate root squared error between two images
-    error = 0
+    error = 0.0
     for row in range(image1.shape[0]):
         for col in range(image1.shape[1]):
             error += (image2[row, col] - image1[row, col]) ** 2
@@ -60,8 +90,15 @@ if __name__ == "__main__":
     random_seed = int(input())
 
     # Generate image based on input function
-    generate_custom_image(function)
+    generated_image = generate_custom_image(function)
+
+    normalized_image = normalize_image(generated_image)
+    quantized_image = quantize_image(normalized_image)
+
+    plt.subplot(211); plt.imshow(quantized_image, cmap="gray")
 
     # Open original image
     original_image = np.load(filename)
-
+    plt.subplot(212); plt.imshow(original_image, cmap="gray")
+    plt.show()
+    print(round(root_squared_error(quantized_image, original_image), 4))
