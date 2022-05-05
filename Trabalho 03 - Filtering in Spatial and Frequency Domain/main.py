8066a41657bc989b421a3c205722276e45e5b0ad
murilo-toddy <murilo.todao@usp.br>
2022-05-05 14:56:30 -0300

    Create Trabalho 03 basic files

---
+++ Trabalho 03 - Filtering in Spatial and Frequency Domain/main.py
@@ -0,0 +1,11 @@
+# Name:   Murilo Fantucci Todão
+# NUSP:   11299982
+# Course: SCC0251
+# Year:   2022
+# Title:  Assignment 3 - Filtering in Spatial and Frequency Domain
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    s = 2
