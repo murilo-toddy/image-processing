734266a4825007be3d05fae86ac45251458d3efc
murilo-toddy <murilo.todao@usp.br>
2022-05-28 12:20:52 -0300

    Create assignment 04 env

---
+++ Trabalho 04 - Image Restoration/main.py
@@ -0,0 +1,11 @@
+# Name: Murilo Fantucci Todão
+# NUSP: 11299982
+# Course: SCC0251
+# Year:   2022
+# Title:  Assignment 4 - Image Restoration
+import numpy as np
+import imageio
+
+
+if __name__ == "__main__":
+    pass
