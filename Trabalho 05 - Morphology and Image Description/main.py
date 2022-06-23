784a9edc7ea0f3a499996273121c94480e4c87d5
murilo-toddy <murilo.todao@usp.br>
2022-06-23 00:52:02 -0300

    Add basic functions

--- Trabalho 05 - Morphology and Image Description/main.py
+++ Trabalho 05 - Morphology and Image Description/main.py
@@ -2,6 +2,36 @@ import numpy as np
 import imageio
 
 
+def luminance_weights(image):
+    return 0.299 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]
+
+
+def limiarization(image):
+    return image
+
+
+def erode_image(image):
+    return image
+
+
+def dilate_image(image):
+    return image
+
+
+def opening(image):
+    eroded_image = erode_image(image)
+    return dilate_image(eroded_image)
+
+
+def closing(image):
+    dilated_image = dilate_image(image)
+    return erode_image(dilated_image)
+
+
+def generate_masks(image):
+    return image, image
+
+
 if __name__ == "__main__":
     # Read user input
     index = int(input().rstrip())
@@ -9,6 +39,18 @@ if __name__ == "__main__":
     f_param = int(input().rstrip())
     t_param = int(input().rstrip())
     dataset_size = int(input().rstrip())
-    image_names = []
+    images = []
     for i in range(dataset_size):
-        image_names[i] = input().rstrip()
+        if i == index:
+            image = imageio.imread(input().rstrip())
+            continue
+        images.append(imageio.imread(input().rstrip()))
+
+    gray_image = luminance_weights(image)
+    limiarized_image = limiarization(gray_image)
+    if f_param == 1:
+        transformed_image = closing(limiarized_image)
+    else:
+        transformed_image = opening(limiarized_image)
+
+    mask1, mask2 = generate_masks(transformed_image)
