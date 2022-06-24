67886d43b5a2b499f237f1b88b0f0b5cdae37b0f
murilo-toddy <murilo.todao@usp.br>
2022-06-24 01:11:10 -0300

    Implement basic functions

---
+++ Trabalho 06 - Color Image Processing and Segmentation/main.py
@@ -0,0 +1,78 @@
+from inspect import CORO_RUNNING
+import numpy as np
+import imageio
+import random
+
+
+def luminance(image):
+    grayscale_image = 0.299 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]
+    return np.clip(grayscale_image.astype(np.uint8), 0, 255)
+
+
+def euclidian_distance(m1, m2):
+    return np.sqrt(np.sum(np.square(m1 - m2)))
+
+
+def get_closest_centroid_index(coordinate, centroids):
+    closest_centroid_index = 0
+    min_distance = euclidian_distance(coordinate, centroids[0])
+    # Calculate the closest centroid and return its index
+    for index, centroid in enumerate(centroids):
+        distance = euclidian_distance(coordinate, centroid)
+        if distance < min_distance:
+            closest_centroid_index = index
+    return closest_centroid_index
+
+
+def k_method(number_of_clusters, image, option, number_of_iters):
+    # Initialize centroids
+    initial_centroids = np.sort(random.sample(range(0, image.shape[0] * image.shape[1]), number_of_clusters))
+    centroids = []
+    for centroid in initial_centroids:
+        centroids.append(np.array([centroid // image.shape[1], centroid % image.shape[1]]))
+    
+    print(centroids)
+    pixels_in_centroid = [[]] * number_of_clusters
+    for _ in range(number_of_iters):
+        for x in range(image.shape[0]):
+            for y in range(image.shape[1]):
+                coordinate = np.array([x, y])
+                # For each pixel, save its closest centroid
+                pixels_in_centroid[get_closest_centroid_index(coordinate, centroids)].append(np.array([x, y]))
+                
+
+    print(pixels_in_centroid)
+
+
+def rmse(image1, image2):
+    m, n = image1.shape
+    return np.sqrt(np.sum((int(image1) - int(image2)) ** 2) / m / n)
+
+
+def colored_rmse(image1, image2):
+    error = 0
+    for i in range(3):
+        error += rmse(image1[:, :, i], image2[:, :, i])
+    return error / 3
+
+
+if __name__ == "__main__":
+    # Read user input
+    input_image_name = input().rstrip()
+    reference_image_name = input().rstrip()
+    option = int(input())
+    number_of_clusters = int(input())
+    number_of_iters = int(input())
+    seed = int(input())
+    random.seed(seed)
+
+    # Open images
+    input_image = imageio.imread(input_image_name)
+    reference_image = imageio.imread(reference_image_name)
+
+    generated_image = k_method(number_of_clusters, input_image, option, number_of_iters)
+
+    if option < 3:
+        print(round(colored_rmse(generated_image, reference_image), 4))
+    else:
+        print(round(rmse(generated_image, reference_image), 4))
---
+++ Trabalho 06 - Color Image Processing and Segmentation/testcases/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 24/06/2022 00:51:59 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Assignment 6 - Color Image Processing and Segmentation
