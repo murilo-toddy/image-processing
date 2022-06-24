554ce505bb51307a8154c605113e41a08a6513a6
murilo-toddy <murilo.todao@usp.br>
2022-06-24 09:38:43 -0300

    Improve commenting

--- Trabalho 06 - Color Image Processing and Segmentation/main.py
+++ Trabalho 06 - Color Image Processing and Segmentation/main.py
@@ -1,18 +1,20 @@
-from inspect import CORO_RUNNING
 import numpy as np
 import imageio
 import random
 
 
+# Convert RGB image to grayscale using luminance
 def luminance(image):
     grayscale_image = 0.299 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]
     return np.clip(grayscale_image.astype(np.uint8), 0, 255)
 
 
+# Calculate euclidian distance between two arrays
 def euclidian_distance(m1, m2):
     return np.sqrt(np.sum(np.square(m1 - m2)))
 
 
+# Find closest centroid to a given pixel
 def get_closest_centroid_index(coordinate, centroids):
     closest_centroid_index = 0
     min_distance = euclidian_distance(coordinate, centroids[0])
@@ -24,6 +26,7 @@ def get_closest_centroid_index(coordinate, centroids):
     return closest_centroid_index
 
 
+# Apply k-Method in image
 def k_method(number_of_clusters, image, option, number_of_iters):
     # Initialize centroids
     initial_centroids = np.sort(random.sample(range(0, image.shape[0] * image.shape[1]), number_of_clusters))
@@ -44,11 +47,13 @@ def k_method(number_of_clusters, image, option, number_of_iters):
     print(pixels_in_centroid)
 
 
+# Calculate difference between single-color images
 def rmse(image1, image2):
     m, n = image1.shape
     return np.sqrt(np.sum((int(image1) - int(image2)) ** 2) / m / n)
 
 
+# Calculate difference between colored images
 def colored_rmse(image1, image2):
     error = 0
     for i in range(3):
@@ -70,6 +75,7 @@ if __name__ == "__main__":
     input_image = imageio.imread(input_image_name)
     reference_image = imageio.imread(reference_image_name)
 
+    # Apply k-method
     generated_image = k_method(number_of_clusters, input_image, option, number_of_iters)
 
     if option < 3:
